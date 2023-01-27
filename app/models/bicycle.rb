5bb72d436157d0a693144404a94e7453174db801
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-01-26 20:48:20 -0600

    delete on bike

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,6 +1,6 @@
 class Bicycle < ApplicationRecord
-    has_many :bike_parts
+    has_many :bike_parts, dependent: :destroy
     has_many :parts, through: :bike_parts
-    has_many :garages
+    has_many :garages, dependent: :destroy
     has_many :users, through: :garages
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,6 +1,6 @@
 class Part < ApplicationRecord
     has_many :bike_parts
     has_many :bicycles, through: :bike_parts
-    has_many :invntories
+    has_many :inventories
     has_many :users, through: :inventories 
 end
